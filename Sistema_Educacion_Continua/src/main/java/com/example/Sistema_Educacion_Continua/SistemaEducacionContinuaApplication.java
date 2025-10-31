0e70356e7124c51e4e35fec102e8fdd0326eda35
mbran <melvin.bran24@itca.edu.sv>
2025-10-31 11:40:52 -0600

    Bases del sistema de inventarios

---
+++ Sistema_Educacion_Continua/src/main/java/com/example/Sistema_Educacion_Continua/SistemaEducacionContinuaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sistema_Educacion_Continua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEducacionContinuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEducacionContinuaApplication.class, args);
+	}
+
+}
---
+++ Sistema_Educacion_Continua/src/test/java/com/example/Sistema_Educacion_Continua/SistemaEducacionContinuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sistema_Educacion_Continua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEducacionContinuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
